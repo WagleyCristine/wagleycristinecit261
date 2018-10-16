e3528ff546a8c60aa8b6c8dcfb33d2c88c56fcbe
WagleyCristine <43319697+WagleyCristine@users.noreply.github.com>
2018-10-16 10:57:43 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+[
+  {
+    "name": "Pablo Honey",
+    "year" : "1993",
+    "sales": "1.7 Mill"
+      
+    }
+  },
+  
+    {
+    "name": "The Bends",
+    "year" : "1995",
+    "sales": "1.6 Mill"
+      
+    }
+  
+  },
+  {
+    "name": "OK Computer",
+    "year" : "1997",
+    "sales": "3.4 Mill"
+    }
+  }
+]
